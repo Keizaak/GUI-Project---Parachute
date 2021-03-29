985ea3bc4ba6918c4b542401b686b578a1a7ab3e
Keizaak <37865090+Keizaak@users.noreply.github.com>
2021-03-29 23:28:37 +0200

    Translation

--- Project_fr_FR.ts
+++ Project_fr_FR.ts
@@ -1,4 +1,101 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="Project_fr_FR">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Parachute</source>
+        <translation>Parachute</translation>
+    </message>
+    <message>
+        <source>Binary array</source>
+        <translation>Tableau binaire</translation>
+    </message>
+    <message>
+        <source>Message:</source>
+        <translation>Message :</translation>
+    </message>
+    <message>
+        <source>Number of sectors</source>
+        <translation>Nombre de secteurs :</translation>
+    </message>
+    <message>
+        <source>Number of tracks</source>
+        <translation>Nombre de pistes :</translation>
+    </message>
+    <message>
+        <source>&amp;File</source>
+        <translation>Fichier</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation>Aide</translation>
+    </message>
+    <message>
+        <source>&amp;Open</source>
+        <translation>Ouvrir</translation>
+    </message>
+    <message>
+        <source>&amp;Save</source>
+        <translation>Sauvegarder</translation>
+    </message>
+    <message>
+        <source>&amp;Quit</source>
+        <translation>Quitter</translation>
+    </message>
+    <message>
+        <source>&amp;About</source>
+        <translation>A propos</translation>
+    </message>
+    <message>
+        <source>Crypted parachute</source>
+        <translation>Parachute crypté</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>A propos</translation>
+    </message>
+    <message>
+        <source>Crypted parachute - GUI Project
+(c) Raphaël ANCETTE &amp; Camille GUIGNOL</source>
+        <translation>Parachute crypté - Projet IG
+        (c) Raphaël ANCETTE &amp; Camille GUIGNOL</translation>
+    </message>
+    <message>
+        <source>Save parachute settings</source>
+        <translation>Sauvegarder les paramètres du parachute</translation>
+    </message>
+    <message>
+        <source>Parachute settings (*.pst);;All Files (*)</source>
+        <translation>Paramètres du parachute</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Sauvegarder</translation>
+    </message>
+    <message>
+        <source>Parachute settings have been saved successfully</source>
+        <translation>Les paramètres du parachute ont été sauvegardés</translation>
+    </message>
+    <message>
+        <source>Parachute settings could not be saved, please try again</source>
+        <translation>Les paramètres du parachute n'ont pas pu être sauvegardés, veuillez réessayer</translation>
+    </message>
+    <message>
+        <source>Open parachute settings</source>
+        <translation>Ouvrir les paramètres du parachute</translation>
+    </message>
+    <message>
+        <source>Load</source>
+        <translation>Charger</translation>
+    </message>
+    <message>
+        <source>Parachute settings have been loaded successfully</source>
+        <translation>Les paramètres du parachute ont été chargés</translation>
+    </message>
+    <message>
+        <source>Parachute settings could not be loaded, your file is invalid</source>
+        <translation>Les paramètres du parachute n'ont pas pu être chargés, votre fichier est invalide</translation>
+    </message>
+</context>
 </TS>
--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include "MainWindow.h"
 
 #include <QApplication>
+#include <QTranslator>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QTranslator translator;
+    translator.load("Project_fr_FR");
+    a.installTranslator(&translator);
+
     MainWindow w;
     w.show();
     return a.exec();
